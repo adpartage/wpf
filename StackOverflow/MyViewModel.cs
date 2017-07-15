a045d55734b9575eeb37f93794315007b0ff8f70
Lans <nothing@na.com>
2017-07-15 11:54:50 +0200

    StackOverFlow

---
+++ StackOverflow/MyViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StackOverflow
+{
+    public class MyViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        public MyViewModel(string textBoxValue)
+        {
+            ChooseGameCommand = new RelayCommand<string>(ChooseGameCommandHandler);
+            TextBoxValue = textBoxValue;
+        }
+
+        public MyViewModel() : this(string.Empty) { }
+
+        public string TextBoxValue { get; set; }
+
+        private bool _isvisible = true;
+        public bool IsVisible
+        {
+            get { return _isvisible; }
+            set
+            {
+                if (_isvisible != value)
+                {
+                    _isvisible = value;
+                    RaisePropertyChanged(nameof(IsVisible));
+                }
+            }
+        }
+
+        private void ChooseGameCommandHandler(string parameter)
+        {
+            var window = new MainWindow();
+            window.DataContext = new MyViewModel(TextBoxValue);
+            window.Show();
+            IsVisible = false;
+        }
+
+        public ICommand ChooseGameCommand { get; private set; }        
+    }
+}
